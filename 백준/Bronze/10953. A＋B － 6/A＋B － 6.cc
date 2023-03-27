34f470a4b7b88747f93278e9afb83d7cc771d537
GaeunU <65946325+gaeunU@users.noreply.github.com>
2023-03-27 22:52:19 +0900

    [Bronze II] Title: A+B - 6, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10953. A＋B － 6/A＋B － 6.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n,A,B;
+    char c;
+    cin>>n;
+    
+    for(int i=0; i<n;i++){
+        cin>>A>>c>>B;
+        cout<<A+B<<endl;
+    }
+}
