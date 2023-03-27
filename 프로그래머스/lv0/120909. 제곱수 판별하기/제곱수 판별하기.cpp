615b03ac79757acdbee43bc34af1d00bd4f186ba
GaeunU <65946325+gaeunU@users.noreply.github.com>
2023-03-27 22:54:51 +0900

    [level 0] Title: 제곱수 판별하기, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120909. 제곱수 판별하기/제곱수 판별하기.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n) {
+    int answer = 0;
+    for(int i=0; i<=1000; i++){
+        if(n == i*i){
+            answer =1;
+            break;
+        }else
+            answer =2;
+    }
+    return answer;
+}
