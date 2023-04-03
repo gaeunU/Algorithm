29c9167f2f5360937373c991bd73ecd154ee73bd
GaeunU <65946325+gaeunU@users.noreply.github.com>
2023-04-03 14:36:45 +0900

    [Bronze I] Title: 최대공약수와 최소공배수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int gcd(int a, int b){
+    
+    int c;
+    while(b!=0){
+        c=a%b;
+        a=b;
+        b=c;
+    }
+    
+    return a;
+}
+int main()
+{
+    int a,b;
+    
+    cin>>a>>b;
+    int g = gcd(a,b);
+    cout<<g<<"\n";
+    cout<<(a*b)/g<<"\n";
+    return 0;
+}
