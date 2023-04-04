9367d809767fff5260fba5bc1b5c9f20fc0f986e
GaeunU <65946325+gaeunU@users.noreply.github.com>
2023-04-04 18:23:24 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.01 ms, Memory: 4.22 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> numbers) {
+    int answer = -1;
+    int sum = 0;
+    for(int i=0; i<numbers.size();i++){
+        sum += numbers[i];
+    }
+    answer = 45-sum;
+    
+    return answer;
+}
