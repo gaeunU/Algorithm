da324ab8ca790a82816b291faa06d1c844778733
GaeunU <65946325+gaeunU@users.noreply.github.com>
2023-05-09 15:30:35 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string s;
+
+
+int main()
+{
+    int num;
+    string s;
+    
+    cin>>s;
+    cout<<s.size();
+   
+    return 0;
+}
