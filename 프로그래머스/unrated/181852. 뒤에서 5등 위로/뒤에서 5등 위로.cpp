50ef437bd87764f05ab4b19b0874faa55df43493
GaeunU <65946325+gaeunU@users.noreply.github.com>
2023-05-10 11:08:10 +0900

    [unrated] Title: 뒤에서 5등 위로, Time: 0.02 ms, Memory: 4.01 MB -BaekjoonHub

--- 프로그래머스/unrated/181852. 뒤에서 5등 위로/뒤에서 5등 위로.cpp
+++ 프로그래머스/unrated/181852. 뒤에서 5등 위로/뒤에서 5등 위로.cpp
@@ -7,8 +7,11 @@ using namespace std;
 vector<int> solution(vector<int> num_list) {
     vector<int> answer;
     sort(num_list.begin(), num_list.end());
+    /*
     for(int i=0; i<5;i++)
         num_list.erase(num_list.begin());
+    */
+    num_list.erase(num_list.begin(),num_list.begin()+5);
     answer = num_list;
     return answer;
 }
