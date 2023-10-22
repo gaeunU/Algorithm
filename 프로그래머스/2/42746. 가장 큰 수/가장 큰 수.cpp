47f547d8becface4e33b482a8e660d125b597ddb
GaeunU <65946325+gaeunU@users.noreply.github.com>
2023-10-22 11:55:36 +0900

    [level 2] Title: 가장 큰 수, Time: 0.02 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.cpp
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.cpp
@@ -16,14 +16,14 @@ string solution(vector<int> numbers) {
     vector<string> temp;
     //string으로 바꾼후 벡터에 집어넣는다.
     
-    for (auto num : numbers)
-        temp.push_back(to_string(num));
+    for (int i=0; i<numbers.size();i++)
+        temp.push_back(to_string(numbers[i]));
     
     //더할때 오름차순으로 정렬한다.
     sort(temp.begin(), temp.end(), cmp);
     
     //처음 숫자가 0이라면 0을 반환
-    if (temp.at(0) == "0") return "0";
+    if (temp[0] == "0") return "0";
     
     //처음부터 끝까지 문자열 합치고 리턴
     for (auto num : temp)
