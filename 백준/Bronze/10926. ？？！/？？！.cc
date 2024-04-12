a5a4049769e80d8e43350ad7f98b5c607a38af21
GaeunU <65946325+gaeunU@users.noreply.github.com>
2024-04-12 21:57:07 +0900

    [Bronze V] Title: ??!, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.cc
@@ -0,0 +1,19 @@
+#include<iostream>
+#include <vector>
+
+using namespace std;
+
+
+int main()
+{
+	//ios_base::sync_with_stdio(false);
+	ios_base::sync_with_stdio(0); cin.tie(0);	
+	
+	string str;
+
+	cin >> str;
+
+	cout << str << "??!";
+
+	return 0;
+}
