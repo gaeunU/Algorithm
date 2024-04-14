5bfdaaf6d359f2393e281a8409aad16e2f27bfc2
GaeunU <65946325+gaeunU@users.noreply.github.com>
2024-04-14 15:02:43 +0900

    [Bronze V] Title: 고양이, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.cc
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	//ios_base::sync_with_stdio(false);
+	ios_base::sync_with_stdio(0); cin.tie(0);
+
+	printf("\\    /\\\n");
+	printf(" )  ( ')\n");
+	printf("(  /  )\n");
+	printf(" \\(__)|\n");
+
+	
+	return 0;
+}
