c1beece5c9d33dbe03ee6451d822928b1e76f2a8
GaeunU <65946325+gaeunU@users.noreply.github.com>
2024-04-14 23:01:20 +0900

    [Bronze V] Title: 개, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.cc
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	//ios_base::sync_with_stdio(false);
+	ios_base::sync_with_stdio(0); cin.tie(0);
+
+	printf("|\\_/|\n");
+	printf("|q p|   /}\n");
+	printf("( 0 )\"\"\"\\\n");
+	printf("|\"^\"`    |\n");
+	printf("||_/=\\\\__|\n");
+
+	return 0;
+}
