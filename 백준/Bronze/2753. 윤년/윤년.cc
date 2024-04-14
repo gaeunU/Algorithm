b71559b7d8730aefe9d36900d087756dcd1795fa
GaeunU <65946325+gaeunU@users.noreply.github.com>
2024-04-14 15:05:30 +0900

    [Bronze V] Title: 윤년, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2753. 윤년/윤년.cc
@@ -0,0 +1,28 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	//ios_base::sync_with_stdio(false);
+	ios_base::sync_with_stdio(0); cin.tie(0);
+
+	int n;
+
+	cin >>n;
+
+	if (n % 4 == 0 && n % 100 != 0) {
+		cout << "1";
+		return 0;
+	}
+	else if (n % 4 == 0 && n % 400 == 0) {
+		cout << "1";
+		return 0;
+	}
+	else {
+		cout << "0";
+	}
+
+	
+	return 0;
+}
